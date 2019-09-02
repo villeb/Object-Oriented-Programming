6a1542ba3a5915470ba1f746b01a3b4008c9f044
Vilhelm Burevik Sandberg <49153075+villeb@users.noreply.github.com>
2019-09-02 13:14:51 -0700

    Add files via upload

---
+++ Labb2/mybutton/MyButton2.java
@@ -0,0 +1,48 @@
+
+package mybutton;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+ /**
+ *
+ * @author Vilhelm
+ */
+
+public class MyButton2 extends JButton implements ActionListener{
+    //Nödvändiga fält
+    private Color c1;
+    private Color c2;
+    private String firsttext;
+    private String sndtext;
+
+    //Konstruktor som sätter kanppen till första tillstådent och färg,text
+    public MyButton2(Color col1, Color col2, String text1, String text2) {
+        setText(text1);
+        setBackground(col1);
+        c1 = col1;
+        c2 = col2;
+        firsttext = text1;
+        sndtext = text2;
+        addActionListener(this);
+
+    }
+
+    public MyButton2() {
+        this(Color.GREEN, Color.RED, "ON", "OFF");
+    }
+    //Används för att byta tillstånd, knapp 
+    public void toggleState() {
+        if (getBackground() == c1) {
+            setBackground(c2);
+            setText(sndtext);
+        } else {
+            setBackground(c1);
+            setText(firsttext);
+        }
+    }
+
+     public void actionPerformed(ActionEvent e) {
+        toggleState();
+     }
+    
+}
